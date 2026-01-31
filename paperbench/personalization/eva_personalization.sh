555333b4c2fef4688ebe75ba829bd2c2246cb3a5
Shangding Gu <gshangd@163.com>
2026-01-31 15:35:21 -0800

    Add support for closed source models in script

--- paperbench/personalization/eva_personalization.sh
+++ paperbench/personalization/eva_personalization.sh
@@ -6,3 +6,13 @@ python eval_personalization_mcq.py \
   --models "meta-llama/Llama-4-Scout-17B-16E-Instruct" \
   --temperature 0.0 \
   --max_tokens 8
+
+# for closed source models
+export USE_OPENAI=0
+export LITELLM_BASE_URL="your_litellm_link/v1"
+export LITELLM_API_KEY="sk-XXX"
+python /home/shangdinggu/nvidia_server/agentic_web/live_rl/opal_benchmark/personal/build_2stage_data/eval_personalization_litllm.py \
+  --input your_data.jsonl \
+  --outdir your_results/ \
+  --models "openai/gpt-5.2" \
+  --temperature 0.0 --max_tokens 8 \
